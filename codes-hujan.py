e406692eeabf74464d0ad4345ffab0f8ffec4fe9
ErsaScarletZ <ameerfaisal76@gmail.com>
2017-12-28 08:55:06 +0800

    Fixed graph not lie on it axis perfectly

--- codes-hujan.py
+++ codes-hujan.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import skfuzzy as fuzz
 x_rainfall = np.arange(0, 300, 1)
-rainfall_low = fuzz.trimf(x_rainfall, [0, 61, 1])
-rainfall_low = fuzz.trimf(x_rainfall, [0, 0, 61])
-rainfall_moderate = fuzz.trimf(x_rainfall, [61, 183, 183])
-rainfall_high = fuzz.trimf(x_rainfall, [183, 200, 300])
+rainfall_low = fuzz.trimf(x_rainfall, [0, 30, 61])
+rainfall_moderate = fuzz.trimf(x_rainfall, [40, 122, 183])
+rainfall_high = fuzz.trimf(x_rainfall, [160, 200, 300])
 fig, ax = plt.subplots()
+plt.axis([0, 300, 0, 1])
 ax.set_title('Jumlah Hujan')
 ax.plot(x_rainfall, rainfall_low, linewidth=1.5, label='low')
 ax.plot(x_rainfall, rainfall_moderate, linewidth=1.5, label='moderate')
---
+++ codes-river.py
@@ -0,0 +1,24 @@
+#importing support library and dependencies
+import numpy as np
+import skfuzzy as fuzz
+import matplotlib .pyplot as plt
+
+
+#setting up graph for membership value
+x_riverlevel = np.arange(0, 6, 1)
+riverlevel_normal = fuzz.trimf(x_riverlevel, [0,1,2])
+riverlevel_berjaga = fuzz.trimf(x_riverlevel, [0,2,3])
+riverlevel_amaran = fuzz.trimf(x_riverlevel, [1,3,4])
+riverlevel_bahaya = fuzz.trimf(x_riverlevel, [3,4,5])
+fig, ax = plt.subplots()
+plt.axis([0, 5, 0, 1])#the graph lie perfectly on the axis
+ax.set_title('River level')#graph title
+
+#plotting membership value
+ax.plot(x_riverlevel, riverlevel_normal,linewidth=1.5, label='normal')
+ax.plot(x_riverlevel, riverlevel_berjaga,linewidth=1.5, label='berjaga')
+ax.plot(x_riverlevel, riverlevel_amaran,linewidth=1.5, label='amaran')
+ax.plot(x_riverlevel, riverlevel_bahaya,linewidth=1.5, label='bahaya')
+ax.legend()
+#display the graph windows
+plt.show()
