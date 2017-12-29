1964fcb754a7f697245d59ddc13d92a6fbbab620
ErsaScarletZ <ameerfaisal76@gmail.com>
2017-12-29 09:16:55 +0800

    Complted FIS. If Else Broken

--- rules.py
+++ rules.py
@@ -3,8 +3,8 @@ import skfuzzy as fuzz
 import matplotlib .pyplot as plt
 
 
-CURRENT_RIVER_LEVEL = 4.4
-CURRENT_RAINFALL = 280
+CURRENT_RIVER_LEVEL = 3
+CURRENT_RAINFALL = 100
 x_riverlevel = np.arange(0, 6, 1)
 x_rainfall = np.arange(0, 300, 1)
 x_floodstatus = np.arange(0, 10, 1)
@@ -19,9 +19,9 @@ print("x_floodstatus"+ str(x_floodstatus))
 #river membership value
 riverlevel_normal = fuzz.trimf(x_riverlevel, [0,1,2])
 print("riverlevel_normal: " + str(riverlevel_normal))
-riverlevel_berjaga = fuzz.trimf(x_riverlevel, [0,2,3])
+riverlevel_berjaga = fuzz.trimf(x_riverlevel, [1,2,3])
 print("riverlevel_berjaga: " + str(riverlevel_berjaga))
-riverlevel_amaran = fuzz.trimf(x_riverlevel, [1,3,4])
+riverlevel_amaran = fuzz.trimf(x_riverlevel, [2,3,4])
 print("riverlevel_amaran: " + str(riverlevel_amaran))
 riverlevel_bahaya = fuzz.trimf(x_riverlevel, [3,4,5])
 print("riverlevel_bahaya: " + str(riverlevel_bahaya))
@@ -35,19 +35,29 @@ print("rainfall_moderate: " + str(rainfall_moderate))
 rainfall_high = fuzz.trimf(x_rainfall, [160, 200, 300])
 print("rainfall_high: " + str(rainfall_high))
 
-#floodstatus membership value
+"""#floodstatus membership value using trimf
 flood_none = fuzz.trimf(x_floodstatus, [0, 1, 1])
 print("flood_none: " + str(flood_none))
 flood_minor = fuzz.trimf(x_floodstatus, [0.5, 3, 4])
 print("flood_minor: " + str(flood_minor))
 flood_moderate = fuzz.trimf(x_floodstatus, [3, 5, 6])
 print("flood_moderate: " + str(flood_moderate))
-flood_major = fuzz.trimf(x_floodstatus, [5.5, 7, 8])
-print("flood_major: " + str(flood_major))
-"""flood_extreme = fuzz.trimf(x_floodstatus, [7.5, 9, 10])
-print("flood_extreme: " + str(flood_extreme))"""
+flood_major = fuzz.trimf(x_floodstatus, [5.5, 7, 9])
+print("flood_major: " + str(flood_major))"""
 
-#visualization of the membership value
+###############################################################
+#floodstatus using trapezoidal mf
+flood_none = fuzz.trapmf(x_floodstatus, [0, 1, 2, 3])
+print("flood_none: " + str(flood_none))
+flood_minor = fuzz.trapmf(x_floodstatus, [1, 3, 4, 5])
+print("flood_minor: " + str(flood_minor))
+flood_moderate = fuzz.trimf(x_floodstatus, [4, 5, 6])
+print("flood_moderate: " + str(flood_moderate))
+flood_major = fuzz.trapmf(x_floodstatus, [5.5, 6, 7, 9])
+print("flood_major: " + str(flood_major))
+
+
+######################visualization of the membership value##############################################
 #River level
 fig, ax = plt.subplots()
 ax.set_title('River Level')
@@ -57,7 +67,7 @@ ax.plot(x_riverlevel, riverlevel_berjaga,linewidth=1.5, label='berjaga')
 ax.plot(x_riverlevel, riverlevel_amaran,linewidth=1.5, label='amaran')
 ax.plot(x_riverlevel, riverlevel_bahaya,linewidth=1.5, label='bahaya')
 ax.legend()
-plt.axis([0, 5, 0, 1])
+plt.axis([0, 5, 0, 2])
 #plt.show()
 
 #Rainfall
@@ -68,7 +78,7 @@ ax.plot(x_rainfall, rainfall_low, linewidth=1.5, label='low')
 ax.plot(x_rainfall, rainfall_moderate, linewidth=1.5, label='moderate')
 ax.plot(x_rainfall, rainfall_high, linewidth=1.5, label='heavy')
 ax.legend()
-plt.axis([0, 300, 0, 1])
+plt.axis([0, 300, 0, 2])
 #plt.show()
 
 #Flood Status
@@ -81,10 +91,11 @@ ax.plot(x_floodstatus, flood_moderate, linewidth=1.5, label='Moderate Flood')
 ax.plot(x_floodstatus, flood_major, linewidth=1.5, label='Major Flood')
 """ax.plot(x_floodstatus, flood_extreme, linewidth=1.5, label='Extreme Flood')"""
 ax.legend()
-plt.axis([0, 10, 0, 1])
-plt.show()
+plt.axis([0, 10, 0, 2])
+#plt.show()
 
-#activation function for those value
+################################################################################################################################
+#########################################activation function for those value####################################################
 river_level_norm = fuzz.interp_membership(x_riverlevel, riverlevel_normal, CURRENT_RIVER_LEVEL)
 print("river_level_norm: " + str(river_level_norm))
 river_level_berjaga = fuzz.interp_membership(x_riverlevel, riverlevel_berjaga, CURRENT_RIVER_LEVEL)
@@ -107,24 +118,70 @@ results = dict()
 #rules evaluation
 
 #in min-max algorithm, fmin is equivalent to AND, fmax is equivalent to OR
+
+######################################################################################################
 #Rule 1 no flood -> river level normal
 active_flood_none = np.fmin(river_level_norm, flood_none)
-active_rule1 = river_level_norm
-#print("\nactive_rule1: " + str(active_rule1))
 print("\nFlood None: " + str(active_flood_none))
 #results['active_rule1'] = active_rule1
 
+######################################################################################################
 #Rule 2 minor flood -> rainfall moderate + river level berjaga || normal
 combined_river_level1 = np.fmax(river_level_berjaga, river_level_norm) # level normal OR berjaga
-#print("\nRiver_level_combined: " + str(combined_river_level))
-active_flood_minor = np.fmin(rainfall_moderate, combined_river_level1) # AND moderate rainfall
+#print("\nRiver_level_combined: " + str(combined_river_level1))
+combined_attrib = np.fmin(rainfall_moderate, combined_river_level1) # AND moderate rainfall
+active_flood_minor = np.fmin(combined_attrib, flood_minor)
 print("\nFlood Minor: " + str(active_flood_minor))
 
-#Rule 3 major flood -> rainfall moderate || high + river level amaran || bahaya
-combined_river_level2 = np.fmax(river_level_amaran, river_level_bahaya) # level normal OR berjaga
-#print("\nRiver_level_combined: " + str(combined_river_level))
-combined_rainfall = np.fmax(rainfall_moderate, rainfall_high)
 
-active_flood_major = np.fmin(combined_rainfall, combined_river_level2) # AND moderate rainfall
+######################################################################################################
+#Rule 3 moderate flood -> rainfall moderate + river level berjaga || amaran
+combined_river_level3 = np.fmax(river_level_berjaga, river_level_amaran) # level normal OR berjaga
+#print("\nRiver_level_combined: " + str(combined_river_level2))
+
+combined_attrib2 = np.fmin(rainfall_moderate, combined_river_level3) # AND moderate rainfall
+active_flood_moderate = np.fmin(rainfall_moderate, flood_moderate)
+print("\nFlood Moderate: " + str(active_flood_moderate))
+
+######################################################################################################
+#Rule 4 major flood -> rainfall high + river level amaran || bahaya                     
+combined_river_level2 = np.fmax(river_level_amaran, river_level_bahaya) # level normal OR berjaga
+#print("\nRiver_level_combined: " + str(combined_river_level2))
+
+combined_attrib3 = np.fmin(rainfall_high, combined_river_level2) # AND moderate rainfall
+active_flood_major = np.fmin(combined_attrib3, flood_major)
 print("\nFlood Major: " + str(active_flood_major))
 
+#Rules aggregation
+aggregated = np.fmax(active_flood_none, np.fmax(active_flood_minor, np.fmax(active_flood_moderate, active_flood_major)))
+print("\nAggregated Value: " + str(aggregated))
+
+
+
+#calculating defuzzified value
+#using centroid method
+flood_stats = fuzz.defuzz(x_floodstatus, aggregated, 'centroid')
+#using bisector
+#flood_stats2 = fuzz.defuzz(x_floodstatus, aggregated, 'bisector')
+#using mean of max
+#flood_stats3 = fuzz.defuzz(x_floodstatus, aggregated, 'som')
+#using max of maximum
+#flood_stats4 = fuzz.defuzz(x_floodstatus, aggregated, 'lom')
+print("\nFlood Status: " + str(flood_stats))
+#print("\nFlood Status: " + str(flood_stats2))
+#print("\nFlood Status: " + str(flood_stats3))
+#print("\nFlood Status: " + str(flood_stats4))
+
+
+if flood_stats >0:
+    if flood_stats <=2:
+        print("No Flood. Check River Level")
+elif flood_stats >2:
+    if flood_stats <=4:
+        print("Minor Flood might happen. Check river level for high tides")
+elif flood_stats >4:
+    if flood_stats <=6:
+        print("Moderate flood. Prepare for evacuation.")
+elif flood_stats >6:
+    if flood_stats <=9:
+        print("Major flood. Code Red. Evacuation is a must")
